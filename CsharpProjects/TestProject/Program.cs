67128567915a938791e5a7c37162848b427f7d8a
joaolimareis <victorreislima186@gmail.com>
2025-05-07 22:26:26 -0300

    Armazenar e iterar em sequências de dados usando Matrizes e a instrução foreach em C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -28,7 +28,7 @@
 //         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
 //         total += 2;
 //     }
-    
+
 //     Console.WriteLine($"Your total including the bonus: {total}");
 // }
 
@@ -47,27 +47,67 @@
 // }
 
 //Random random = new Random();
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-Console.WriteLine(daysUntilExpiration);
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+// Console.WriteLine(daysUntilExpiration);
 
-if (daysUntilExpiration == 0){
-    Console.WriteLine("Your subscription has expired.");
-}
-else if(daysUntilExpiration == 1){
-    Console.WriteLine(@"Your subscription expires within a day!
-Renew now and save 20%!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <=5 ){
-    Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
-Renew now and save 10%!");
-     discountPercentage =10;
-}
-else if(daysUntilExpiration <=10 ){
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-if (daysUntilExpiration > 0 ){
-    Console.WriteLine($"Voce recebeu um desconto de: {discountPercentage}% ");
+// if (daysUntilExpiration == 0){
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if(daysUntilExpiration == 1){
+//     Console.WriteLine(@"Your subscription expires within a day!
+// Renew now and save 20%!");
+//     discountPercentage = 20;
+// }
+// else if (daysUntilExpiration <=5 ){
+//     Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
+// Renew now and save 10%!");
+//      discountPercentage =10;
+// }
+// else if(daysUntilExpiration <=10 ){
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+// if (daysUntilExpiration > 0 ){
+//     Console.WriteLine($"Voce recebeu um desconto de: {discountPercentage}% ");
+// }
+
+
+
+// string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+
+// // fraudulentOrderIDs[3] = "D000";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// string[] names = ["Rowena", "Robin", "Bao"];
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// int[] inventory = [200, 450, 700, 175, 250];
+// int sum = 0;
+// int bin = 0;
+// foreach (int items in inventory){
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+string[] array = ["B123","C234","A345","C15","B177","G3003","C235","B179"];
+foreach(string i in array){
+    if(i.StartsWith("B")){
+        Console.WriteLine(i);
+    }
+
 }
